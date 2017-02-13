d7a1e95618de35da7141fa97ecb0892c41c58375
Dale Higgs <dale3h@gmail.com>
2017-02-13 15:59:20 -0600

    I forgot to commit this

--- index.js
+++ index.js
@@ -10,61 +10,66 @@ var HomeAssistant = require('./lib/home-assistant');
 var config        = require('./config.json');
 
 var defaults = {
-  "apiUrl": "http://localhost:8123/api",
-  "apiPassword": null,
-  "defaultHidden": true,
-  "keyDateAdded": "date_added",
-  "keyDateUpdated": "date_updated",
-  "customizeFile": "~/.homeassistant/customize.yaml",
-  "customizeFileOutput": null,
-  "debug": false
+  api_url: 'http://localhost:8123/api',
+  api_password: null,
+  default_hidden: true,
+  key_date_added: 'date_added',
+  key_date_updated: 'date_updated',
+  customize_file: '~/.homeassistant/customize.yaml',
+  customize_file_output: null,
+  debug: false,
+  supported_types: ['automation', 'cover', 'group', 'input_boolean', 'input_slider', 'light', 'media_player', 'scene', 'script', 'switch']
 };
 
 var config = deepExtend({}, defaults, config);
 
-if (!config.apiUrl) {
-  throw new Error('Parameter `apiUrl` is missing in config.json');
+if (!config.api_url) {
+  throw new Error('Parameter `api_url` is missing in config.json');
 }
 
-if (!config.customizeFile) {
-  throw new Error('Parameter `customizeFile` is missing in config.json');
+if (!config.customize_file) {
+  throw new Error('Parameter `customize_file` is missing in config.json');
 }
 
-if ('undefined' === typeof config.customizeFileBackup) {
-  config.customizeFileBackup = config.customizeFile + '.backup';
+if ('undefined' === typeof config.customize_file_backup) {
+  config.customize_file_backup = config.customize_file + '.backup';
 }
 
-if (!config.customizeFileOutput) {
-  config.customizeFileOutput = config.customizeFile;
+if (!config.customize_file_output) {
+  config.customize_file_output = config.customize_file;
 }
 
-if (config.customizeFile) {
-  config.customizeFile = config.customizeFile.replace(/^~/, process.env['HOME']);
+if (config.customize_file) {
+  config.customize_file = config.customize_file.replace(/^~/, process.env['HOME']);
 }
 
-if (config.customizeFileOutput) {
-  config.customizeFileOutput = config.customizeFileOutput.replace(/^~/, process.env['HOME']);
+if (config.customize_file_output) {
+  config.customize_file_output = config.customize_file_output.replace(/^~/, process.env['HOME']);
 }
 
-if (config.customizeFileBackup) {
-  config.customizeFileBackup = config.customizeFileBackup.replace(/^~/, process.env['HOME']);
+if (config.customize_file_backup) {
+  config.customize_file_backup = config.customize_file_backup.replace(/^~/, process.env['HOME']);
 }
 
-var hass = new HomeAssistant(config.apiUrl, config.apiPassword);
+var hass = new HomeAssistant(config.api_url, config.api_password);
 
 function entityDomain(x) {
   return x.entity_id.split('.', 1)[0];
 }
 
-function isSupported(x) {
-  return -1 !== ['light', 'switch', 'group', 'scene', 'media_player', 'input_boolean', 'script'].indexOf(entityDomain(x));
+function entityName(x) {
+  return x.entity_id.split('.').slice(1).join('.');
+}
+
+function supportsHaaska(x) {
+  return -1 !== config.supported_types.indexOf(entityDomain(x));
 }
 
 function isHidden(x) {
   var hidden = x.attributes.haaska_hidden;
 
   if ('undefined' === typeof hidden) {
-    hidden = config.defaultHidden;
+    hidden = config.default_hidden;
   } else if (!hidden || 'no' === hidden || 'false' === hidden || '0' === hidden) {
     hidden = false;
   } else {
@@ -72,6 +77,24 @@ function isHidden(x) {
   }
 
   return hidden;
+}
+
+function getFriendlyName(x) {
+  var friendlyName = x.attributes.friendly_name;
+
+  try {
+    if ('undefined' === typeof friendlyName) {
+      friendlyName = entityName(x);
+    }
+
+    if (friendlyName.match(/[_\.]/) && friendlyName.toLowerCase() === friendlyName) {
+      friendlyName = toTitleCase(friendlyName.replace(/[_\.]/g, ' '));
+    }
+  } catch (ex) {
+    friendlyName = null;
+  }
+
+  return friendlyName;
 }
 
 function toTitleCase(str) {
@@ -86,12 +109,12 @@ function currentTime() {
   return date.toISOString().slice(0, 19).replace('T', ' ');
 }
 
-if (!config.debug && config.customizeFileBackup) {
-  fs.writeFileSync(config.customizeFileBackup, fs.readFileSync(config.customizeFile));
+if (!config.debug && config.customize_file_backup) {
+  fs.writeFileSync(config.customize_file_backup, fs.readFileSync(config.customize_file));
 }
 
 var haaska    = {};
-var customize = yaml.safeLoad(fs.readFileSync(config.customizeFile, 'utf8'));
+var customize = yaml.safeLoad(fs.readFileSync(config.customize_file, 'utf8'));
 
 hass.get('states', function(err, res, body) {
   if (err) {
@@ -101,33 +124,35 @@ hass.get('states', function(err, res, body) {
   for (var i = 0; i < body.length; i++ ) {
     var x = body[i];
 
-    if (!isSupported(x)) {
-      continue;
+    haaska[x.entity_id] = {};
+
+    if (supportsHaaska(x)) {
+      haaska[x.entity_id]['haaska_hidden'] = isHidden(x);
     }
 
-    var hidden = isHidden(x);
+    var friendlyName = getFriendlyName(x);
 
-    haaska[x.entity_id] = {
-      haaska_hidden: hidden
-    };
+    if (friendlyName) {
+      haaska[x.entity_id]['friendly_name'] = friendlyName;
+    }
   }
 
   deepExtend(haaska, customize);
 
-  if (config.keyDateAdded || config.keyDateUpdated) {
+  if (config.key_date_added || config.key_date_updated) {
     for (var entityId in haaska) {
       var entityNew = haaska[entityId];
       var entityOld = customize[entityId];
 
-      if (config.keyDateAdded && 'undefined' === typeof entityOld) {
-        entityNew[config.keyDateAdded] = currentTime();
+      if (config.key_date_added && 'undefined' === typeof entityOld) {
+        entityNew[config.key_date_added] = currentTime();
       }
 
-      if (config.keyDateUpdated) {
+      if (config.key_date_updated) {
         try {
           assert.deepEqual(entityOld, entityNew);
         } catch (ex) {
-          entityNew[config.keyDateUpdated] = currentTime();
+          entityNew[config.key_date_updated] = currentTime();
         }
       }
     }
@@ -188,7 +213,7 @@ hass.get('states', function(err, res, body) {
   if (config.debug) {
     console.log(output);
   } else {
-    fs.writeFileSync(config.customizeFileOutput, output, 'utf8');
-    console.log('%s has been updated', config.customizeFileOutput);
+    fs.writeFileSync(config.customize_file_output, output, 'utf8');
+    console.log('%s has been updated', config.customize_file_output);
   }
 });
